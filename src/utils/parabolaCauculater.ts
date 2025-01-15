869cbbe313614f49b65d532af4a06c1c33166f1c
YOAKE <yo2yoake@outlook.com>
2025-01-15 15:10:27 +0800

    refactor(parabola cauculater): simplify code

--- src/utils/parabolaCauculater.ts
+++ src/utils/parabolaCauculater.ts
@@ -1,45 +1,73 @@
 import * as Cesium from 'cesium'
 
-const getParabola = (startPoint: [number, number], endPoint: [number, number], ratio = 0.1) => {
+const getParabola = (startPoint: [number, number], endPoint: [number, number]) => {
 
-  // 方程 y=-(4h/L^2)*x^2+h h:顶点高度 L：横纵间距较大者
-  const h = Cesium.Cartesian3.distance(
+  // 该算法的原理为抛物线方程 y=-(4h/L^2)*x^2+H 。式中H为抛物线的顶点高度，L为抛物线的跨度，即从起点到终点的水平距离。
+  // h和L均已知，则可以根据抛物线方程求出抛物线上任意一点的坐标。
+
+  // 计算顶点高度H，为两点之间的距离的0.1倍
+  const H = Cesium.Cartesian3.distance(
     Cesium.Cartesian3.fromDegrees(startPoint[0], startPoint[1]),
     Cesium.Cartesian3.fromDegrees(endPoint[0], endPoint[1]),
-  ) * ratio // 计算顶点高度
-  const L = Math.abs(startPoint[0] - endPoint[0]) > Math.abs(startPoint[1] - endPoint[1])
-    ? Math.abs(startPoint[0] - endPoint[0])
-    : Math.abs(startPoint[1] - endPoint[1]) // 计算横纵间距较大者
+  ) * 0.1
 
-  const num = 29 // 分段数
-  const result: [number, number, number][] = [] // 存储结果的数组
-  let dlt = L / num // 每段的长度
+  // 将路径分为 n 段
+  const n = 29
+
+  // 结果数组，初始化添加起点坐标
+  const result: [number, number, number][] = [
+    [startPoint[0], startPoint[1], 0]
+  ]
 
   if (Math.abs(startPoint[0] - endPoint[0]) > Math.abs(startPoint[1] - endPoint[1])) {
-    // 以 lng 为基准
-    const delLat = (endPoint[1] - startPoint[1]) / num // 每段的纬度增量
-    if (startPoint[0] - endPoint[0] > 0) dlt = -dlt // 如果起点经度大于终点经度，反向增量
-    for (let i = 0; i < num; i++) {
-      const tempH = h - (Math.pow(-0.5 * L + Math.abs(dlt) * i, 2) * 4 * h) / Math.pow(L, 2) // 计算当前高度
-      const lng = startPoint[0] + dlt * i // 当前经度
-      const lat = startPoint[1] + delLat * i // 当前纬度
-      result.push([lng, lat, tempH]) // 将结果添加到数组中
-    }
-  } else {
-    // 以 lat 为基准
-    const delLng = (endPoint[0] - startPoint[0]) / num // 每段的经度增量
-    if (startPoint[1] - endPoint[1] > 0) dlt = -dlt // 如果起点纬度大于终点纬度，反向增量
-    for (let i = 0; i < num; i++) {
-      const tempH = h - (Math.pow(-0.5 * L + Math.abs(dlt) * i, 2) * 4 * h) / Math.pow(L, 2) // 计算当前高度
-      const lng = startPoint[0] + delLng * i // 当前经度
-      const lat = startPoint[1] + dlt * i // 当前纬度
-      result.push([lng, lat, tempH]) // 将结果添加到数组中
-    }
-  }
-  // 落地
-  result.push([endPoint[0], endPoint[1], 0]) // 添加终点
+    // 当经度之差大于纬度之差时，选取经度作为基准
 
-  return result // 返回结果
+    // 计算每段的经度增量作为步长
+    const L = Math.abs(startPoint[0] - endPoint[0])
+    let step = L / n
+
+    // 计算每段的纬度增量
+    const delLat = (endPoint[1] - startPoint[1]) / n
+
+    // 如果起点经度大于终点经度，反向计算
+    if (startPoint[0] - endPoint[0] > 0) step = -step
+
+    for (let i = 1; i < n; i++) {
+      // 计算当前高度
+      const h = H - (Math.pow(-0.5 * L + Math.abs(step) * i, 2) * 4 * H) / Math.pow(L, 2)
+
+      // 一次循环后的经度，此时的step为经度增量
+      const lng = startPoint[0] + step * i
+
+      // 一次循环后的纬度
+      const lat = startPoint[1] + delLat * i
+
+      result.push([lng, lat, h])
+    }
+
+  } else {
+    // 当纬度之差大于经度之差时，选取纬度作为基准
+
+    const L = Math.abs(startPoint[1] - endPoint[1])
+    let step = L / n
+
+    const delLng = (endPoint[0] - startPoint[0]) / n
+
+    if (startPoint[1] - endPoint[1] > 0) step = -step
+
+    for (let i = 0; i < n; i++) {
+      const h = H - (Math.pow(-0.5 * L + Math.abs(step) * i, 2) * 4 * H) / Math.pow(L, 2)
+      const lng = startPoint[0] + delLng * i
+      const lat = startPoint[1] + step * i
+      result.push([lng, lat, h])
+    }
+
+  }
+
+  // 添加终点
+  result.push([endPoint[0], endPoint[1], 0])
+
+  return result
 }
 
 export const getParabolaCollection = (odData: { o: [number, number], d: [number, number] }[]) => {
@@ -47,6 +75,6 @@ export const getParabolaCollection = (odData: { o: [number, number], d: [number,
   const parabolaCollection: [number, number, number][][] = odData.map((item: { o: [number, number], d: [number, number] }) => {
     return getParabola(item.o, item.d)
   })
-  
+
   return parabolaCollection
 }
